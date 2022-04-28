127c11a9ec922250fa87110e936a06fa9d34584e
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-04-28 15:17:29 +0900

    p71プログラムを改良した

--- Chapter02/SalesCaluculator/SalesCounter.cs
+++ Chapter02/SalesCaluculator/SalesCounter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,8 @@ namespace SalesCaluculator {
         private List<Sale> _sales; //csvファイルから読み込んだデータ
 
         //コンストラクタ
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
 
         //店舗別売り上げを求める
@@ -25,6 +26,22 @@ namespace SalesCaluculator {
 
 
             return dict;
+        }
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        public static List<Sale> ReadSales(String filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
     }
 }
