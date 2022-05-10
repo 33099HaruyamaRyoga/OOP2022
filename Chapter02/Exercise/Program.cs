d9f07f8aee9a519d738351e87721bc3ee6a57aa8
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-05-10 15:54:45 +0900

    問題2,1.4完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,16 +7,26 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+            //2,1.3
+            var songs = new Song[] {
+                new Song("Habit", "SEKAI NO OWARI", 230),
+                new Song("MAKAFUKA", "RADWIMPS", 331),
+                new Song("幽居のワルツ", "Noel", 233),
+                new Song("The Beginning", "ONE OK ROCK", 293),
+                new Song("お気に召すまま", "Eve", 247)
+            };
 
-            List<Song> songs = new List<Song>();
-
-            Console.WriteLine(" ");
-
-            //PrintSongs(songs);
+            PrintSongs(songs);
         }
-
+        //2,1.4
         private static void PrintSongs(Song[] songs) {
-
+            foreach (var song in songs) {
+                //var span = new TimeSpan(0, 0, song.Length);
+                Console.WriteLine("{0}, {1}, {2:m\\:ss}",
+                   song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
         }
+
+
     }
 }
