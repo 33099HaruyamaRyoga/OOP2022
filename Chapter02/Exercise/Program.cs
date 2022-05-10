9747ffcbf2f79c2284f8c8c71706bb5dd2ef7a53
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-05-10 14:49:40 +0900

    問題2,1.1完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,16 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+
+            List<Song> songs = new List<Song>();
+
+            Console.WriteLine(" ");
+
+            //PrintSongs(songs);
+        }
+
+        private static void PrintSongs(Song[] songs) {
+
         }
     }
 }
