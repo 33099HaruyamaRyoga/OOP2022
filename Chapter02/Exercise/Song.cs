effd8456b2925571b3db8f54f7de1dc77a5c572d
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-05-10 14:50:39 +0900

    問題2,1.2完成

--- Chapter02/Exercise/Song.cs
+++ Chapter02/Exercise/Song.cs
@@ -11,8 +11,12 @@ namespace Exercise {
         public string ArtistName { get; set; } //アーティスト名
         public int Length { get; set; } //演奏時間
 
+        //引数付きコンストラクタ
+        public Song(string title, string artistname, int length) {
 
-
-        
+            this.Title = title;
+            this.ArtistName = artistname;
+            this.Length = length;
+        }
     }
 }
