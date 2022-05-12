a57864e30e1acc37579696461437fd3a5823fe1e
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-05-12 15:54:12 +0900

    while文に変更

--- Chapter02/Exercise03/Program.cs
+++ Chapter02/Exercise03/Program.cs
@@ -11,26 +11,31 @@ namespace Exercise03 {
             int select = 0;
             var sales = new SalesCounter("sales.csv");
 
-            Console.WriteLine("売上高の表示選択");
-            Console.WriteLine("1.店舗別");
-            Console.WriteLine("2.商品カテゴリー別");
-            Console.WriteLine(">");
-
-            select = int.Parse(Console.ReadLine());
             IDictionary<string, int> amountPerStore = null;
-            switch (select) {
-                case 1:
-                    amountPerStore = sales.GetPerStoreSales();
-                    break;
+            
+            while (true) {
+                Console.WriteLine("売上高の表示選択");
+                Console.WriteLine("1.店舗別");
+                Console.WriteLine("2.商品カテゴリー別");
+                Console.WriteLine(">");
 
-                case 2:
-                    amountPerStore = sales.GetPerCategorySales();
-                    break;
-            }
+                select = int.Parse(Console.ReadLine());
+                
+                switch (select) {
+                    case 1:
+                        amountPerStore = sales.GetPerStoreSales();
+                        break;
 
+                    case 2:
+                        amountPerStore = sales.GetPerCategorySales();
+                        break;
 
-            foreach (var obj in amountPerStore) {
-                Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                    case 999:
+                        return;
+                }
+                foreach (var obj in amountPerStore) {
+                    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                }
             }
         }
     }
