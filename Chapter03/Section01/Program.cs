4f44505815daa1c36a2e96b17f0549199d0d6e9d
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-05-13 15:16:55 +0900

    ラムダ演算子

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    class Program {
+        public delegate bool Judgement(int value); //デリゲートの宣言
+
+        static void Main(string[] args) {
+            var list = new List<string> {
+                "Tokyo", "New Delhi", "bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            var names = list.FindAll(s => s.Length <= 5);
+            names.ForEach(s => Console.WriteLine(s));
+
+            /*
+            foreach(var s in names) {
+                Console.WriteLine(s);
+            }
+            */
+            
+        }
+
+    }
+}
