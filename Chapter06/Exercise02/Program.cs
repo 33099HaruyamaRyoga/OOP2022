1dc6cdcdc1ed717c5809ae79943f3cad7b0e8373
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-06-03 13:23:27 +0900

    問題6.2,7 までok!

--- Chapter06/Exercise02/Program.cs
+++ Chapter06/Exercise02/Program.cs
@@ -70,11 +70,17 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_6(List<Book> books) {
-
+            var selected = books.Where(b=> b.Pages >= 400).OrderByDescending(b => b.Price);
+            foreach(var book in selected) {
+                Console.WriteLine("{0} {1}", book.Title, book.Price);
+            }
         }
 
         private static void Exercise2_7(List<Book> books) {
-
+            var selected = books.Where(b => b.Title.Contains("C#") && b.Pages <= 500);
+            foreach(var book in selected) {
+                Console.WriteLine(book.Title);
+            }
         }
 
         class Book {
