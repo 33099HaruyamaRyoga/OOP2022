024b9b63f923e990120e7da2dc436fb5c48f0079
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-06-09 14:04:54 +0900

    NumberGame ok!

--- FormAppSample/NumberGame/Form1.Designer.cs
+++ FormAppSample/NumberGame/Form1.Designer.cs
@@ -24,47 +24,100 @@ namespace NumberGame {
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent() {
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.num1 = new System.Windows.Forms.NumericUpDown();
+            this.Judge = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.num1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num2)).BeginInit();
             this.SuspendLayout();
             // 
-            // numericUpDown1
+            // num1
             // 
-            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.numericUpDown1.Location = new System.Drawing.Point(95, 101);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(120, 36);
-            this.numericUpDown1.TabIndex = 0;
+            this.num1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.num1.Location = new System.Drawing.Point(104, 142);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(120, 36);
+            this.num1.TabIndex = 0;
             // 
-            // button1
+            // Judge
             // 
-            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.button1.Location = new System.Drawing.Point(358, 90);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 56);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "入力";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Judge.Font = new System.Drawing.Font("MS UI Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Judge.Location = new System.Drawing.Point(341, 131);
+            this.Judge.Name = "Judge";
+            this.Judge.Size = new System.Drawing.Size(94, 56);
+            this.Judge.TabIndex = 1;
+            this.Judge.Text = "入力";
+            this.Judge.UseVisualStyleBackColor = true;
+            this.Judge.Click += new System.EventHandler(this.Judge_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(216, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(304, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "の中で好きな数字を入力";
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("MS UI Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tb1.Location = new System.Drawing.Point(104, 235);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(312, 28);
+            this.tb1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(52, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1から";
+            // 
+            // num2
+            // 
+            this.num2.Font = new System.Drawing.Font("MS UI Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.num2.Location = new System.Drawing.Point(130, 40);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(80, 36);
+            this.num2.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(541, 253);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.numericUpDown1);
+            this.ClientSize = new System.Drawing.Size(633, 302);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Judge);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.num1);
             this.Name = "Form1";
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.num1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.num2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown num1;
+        private System.Windows.Forms.Button Judge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown num2;
     }
 }
 
--- FormAppSample/NumberGame/Form1.cs
+++ FormAppSample/NumberGame/Form1.cs
@@ -10,8 +10,28 @@ using System.Windows.Forms;
 
 namespace NumberGame {
     public partial class Form1 : Form {
+
+        private Random rand = new Random();//乱数オブジェクト生成
+
+        //乱数
+        private int randomnumber;
+
         public Form1() {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            //乱数取得
+            randomnumber = rand.Next(minValue: 1, maxValue: (int)num2.Value + 1);
+        }
+
+        private void Judge_Click(object sender, EventArgs e) {
+
+            if(randomnumber == num1.Value) {
+                tb1.Text = ("正解！");
+            } else {
+                tb1.Text = ("不正解！");
+            }
         }
     }
 }
