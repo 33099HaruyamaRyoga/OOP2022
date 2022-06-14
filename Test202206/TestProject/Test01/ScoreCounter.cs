9ce2e1a6b7e956a88656aa80bc7bf55f57578230
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-06-14 14:29:41 +0900

    途中

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,23 +7,32 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
+
+                List<Student> students = new List<Student>();
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines) {
+                    string[] items = line.Split(',');
+                Student student = new Student {
+                        Name = items[0],
+                        subject = items[1],
+                        score = int.Parse(items[2])
+                    };
+                    students.Add(student);
+                }
+                return students;
+            }
 
 
 
 
 
 
-            
         }
 
         //メソッドの概要： 
