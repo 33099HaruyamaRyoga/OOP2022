7946ceb64140450ce2b8cc6249eaef94818b0078
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-07-04 09:41:14 +0900

    RssReader ok

--- Chapter11/RssReader/Form1.Designer.cs
+++ Chapter11/RssReader/Form1.Designer.cs
@@ -28,11 +28,15 @@ namespace RssReader {
             this.cbRssUrl = new System.Windows.Forms.ComboBox();
             this.lbRssTitle = new System.Windows.Forms.ListBox();
             this.wbBrowser = new System.Windows.Forms.WebBrowser();
+            this.btBack = new System.Windows.Forms.Button();
+            this.btFoward = new System.Windows.Forms.Button();
+            this.wvBrowser = new Microsoft.Toolkit.Forms.UI.Controls.WebView();
+            ((System.ComponentModel.ISupportInitialize)(this.wvBrowser)).BeginInit();
             this.SuspendLayout();
             // 
             // btRssGet
             // 
-            this.btRssGet.Location = new System.Drawing.Point(787, 12);
+            this.btRssGet.Location = new System.Drawing.Point(644, 13);
             this.btRssGet.Name = "btRssGet";
             this.btRssGet.Size = new System.Drawing.Size(75, 23);
             this.btRssGet.TabIndex = 0;
@@ -45,11 +49,13 @@ namespace RssReader {
             this.cbRssUrl.FormattingEnabled = true;
             this.cbRssUrl.Location = new System.Drawing.Point(12, 15);
             this.cbRssUrl.Name = "cbRssUrl";
-            this.cbRssUrl.Size = new System.Drawing.Size(769, 20);
+            this.cbRssUrl.Size = new System.Drawing.Size(607, 20);
             this.cbRssUrl.TabIndex = 1;
             // 
             // lbRssTitle
             // 
+            this.lbRssTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
             this.lbRssTitle.FormattingEnabled = true;
             this.lbRssTitle.ItemHeight = 12;
             this.lbRssTitle.Location = new System.Drawing.Point(12, 52);
@@ -67,17 +73,55 @@ namespace RssReader {
             this.wbBrowser.Size = new System.Drawing.Size(609, 628);
             this.wbBrowser.TabIndex = 3;
             // 
+            // btBack
+            // 
+            this.btBack.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btBack.Location = new System.Drawing.Point(742, 5);
+            this.btBack.Name = "btBack";
+            this.btBack.Size = new System.Drawing.Size(30, 31);
+            this.btBack.TabIndex = 4;
+            this.btBack.Text = "◀";
+            this.btBack.UseVisualStyleBackColor = true;
+            this.btBack.Click += new System.EventHandler(this.btBack_Click);
+            // 
+            // btFoward
+            // 
+            this.btFoward.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btFoward.Location = new System.Drawing.Point(778, 5);
+            this.btFoward.Name = "btFoward";
+            this.btFoward.Size = new System.Drawing.Size(30, 31);
+            this.btFoward.TabIndex = 4;
+            this.btFoward.Text = "▶";
+            this.btFoward.UseVisualStyleBackColor = true;
+            this.btFoward.Click += new System.EventHandler(this.btFoward_Click);
+            // 
+            // wvBrowser
+            // 
+            this.wvBrowser.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.wvBrowser.Location = new System.Drawing.Point(253, 52);
+            this.wvBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wvBrowser.Name = "wvBrowser";
+            this.wvBrowser.Size = new System.Drawing.Size(591, 628);
+            this.wvBrowser.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(874, 708);
+            this.Controls.Add(this.wvBrowser);
+            this.Controls.Add(this.btFoward);
+            this.Controls.Add(this.btBack);
             this.Controls.Add(this.wbBrowser);
             this.Controls.Add(this.lbRssTitle);
             this.Controls.Add(this.cbRssUrl);
             this.Controls.Add(this.btRssGet);
             this.Name = "Form1";
             this.Text = "RSSReader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.wvBrowser)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -88,6 +132,9 @@ namespace RssReader {
         private System.Windows.Forms.ComboBox cbRssUrl;
         private System.Windows.Forms.ListBox lbRssTitle;
         private System.Windows.Forms.WebBrowser wbBrowser;
+        private System.Windows.Forms.Button btBack;
+        private System.Windows.Forms.Button btFoward;
+        private Microsoft.Toolkit.Forms.UI.Controls.WebView wvBrowser;
     }
 }
 
--- Chapter11/RssReader/Form1.cs
+++ Chapter11/RssReader/Form1.cs
@@ -12,7 +12,7 @@ using System.Xml.Linq;
 
 namespace RssReader {
     public partial class Form1 : Form {
-        IEnumerable<string> xTitle;
+        IEnumerable<string> xTitle,xLink;
         public Form1() {
             InitializeComponent();
         }
@@ -24,8 +24,8 @@ namespace RssReader {
                 var stream = wc.OpenRead(cbRssUrl.Text);
 
                 var xdoc = XDocument.Load(stream);
-                var xTitle = xdoc.Root.Descendants("item").Select(x => (string)x.Element("title"));
-                var xLink = xdoc.Root.Descendants("item").Select(x => (string)x.Element("link"));
+                xTitle = xdoc.Root.Descendants("item").Select(x => (string)x.Element("title"));
+                xLink = xdoc.Root.Descendants("item").Select(x => (string)x.Element("link"));
 
                 foreach (var data in xTitle) {
 
@@ -34,8 +34,32 @@ namespace RssReader {
             }
         }
 
+        private void btBack_Click(object sender, EventArgs e) {
+            wbBrowser.GoBack();
+        }
+
+        private void btFoward_Click(object sender, EventArgs e) {
+            wbBrowser.GoForward();
+        }
+
+        private void wvBrowser_NavigationCompleted(object sender, Microsoft.Toolkit.Win32.UI.Controls.Interop.WinRT.WebViewControlNavigationCompletedEventArgs e) {
+            btBack.Enabled = wvBrowser.CanGoBack;
+            btFoward.Enabled = wvBrowser.CanGoForward;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            //進む・戻るボタンのマスク設定
+            btBack.Enabled = wvBrowser.CanGoBack;
+            btFoward.Enabled = wvBrowser.CanGoForward;
+        }
+
         private void lsRssTitle_Click(object sender, EventArgs e) {
+            
             int index = lbRssTitle.SelectedIndex; //選択した箇所のインデックスを取得（０～）
+            if (index == -1) return;
+            
+            var url = xLink.ElementAt(index);
+            wvBrowser.Source = new Uri(url);
         }
     }
 }
