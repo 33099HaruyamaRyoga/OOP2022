eed0650cc1fe28056f5c82ea2232fa1e5a82755f
33099HaruyamaRyoga <33099@ojs.ac.jp>
2022-10-13 10:39:30 +0900

    15章　演習問題　終了

--- Chapter15/Chapter15/Excesise01/Program.cs
+++ Chapter15/Chapter15/Excesise01/Program.cs
@@ -37,37 +37,97 @@ namespace Excesise01 {
         }
 
         private static void Exercise1_3() {
-            //var groups = Library.Books
-            //    .GroupBy(b => b.PublishedYear);
-            //var book = 
+            var query = Library.Books
+                .GroupBy(b => b.PublishedYear);
+            foreach(var item in query.OrderBy(b => b.Key)) {
+                Console.WriteLine("{0}年　{1}冊", item.Key, item.Count());
+            }
         }
 
         private static void Exercise1_4() {
-            //var groups = Library.Books
-            //    .GroupBy(b => b.PublishedYear)
-            //    .OrderBy(g => g.Key);
-        //foreach (var book in groups) {
-            //Console.WriteLine($"{book}");
-                
-            //}
-        }
+            var query = Library.Books
+                .Join(Library.Categories,
+                    book => book.CategoryId,
+                    category => category.Id,
+                    (book, category) => new {
+                        book.Title,
+                        book.PublishedYear,
+                        book.Price,
+                        CategoryName = category.Name
+                    })
+                .OrderByDescending(x => x.PublishedYear)
+                .ThenByDescending(x => x.Price);
+            foreach (var item in query)
+                Console.WriteLine("{0}年 {1}円 {2} ({3})",
+                                  item.PublishedYear,
+                                  item.Price,
+                                  item.Title,
+                                  item.CategoryName
+                                  );
+                    }
 
         private static void Exercise1_5() {
-            //var books = Library.Books
-            //      .Where(b => b.PublishedYear == 2016);
-            //Console.WriteLine(books);
+            var quary = Library.Books
+                .Where(b => b.PublishedYear == 2016)
+                .Join(
+                    Library.Categories, book => book.CategoryId,
+                    category => category.Id,
+                    (book, category) => category.Name)
+                .Distinct();
+            foreach (var name in quary)
+                Console.WriteLine(name);
         }
 
         private static void Exercise1_6() {
+            var quary = Library.Books
+                .Join(Library.Categories,
+                    book => book.CategoryId,
+                    category => category.Id,
+                    (book, category) => new {
+                        book.Title,
+                        book.PublishedYear,
+                        book.Price,
+                        CategoryName = category.Name
+                    })
+                .GroupBy(x => x.CategoryName)
+                .OrderBy(x => x.Key);
 
+            foreach(var group in quary) {
+                Console.WriteLine("#{0}", group.Key);
+                foreach(var item in group) {
+                    Console.WriteLine(" {0}", item.Title);
+                }
+            }
         }
 
         private static void Exercise1_7() {
+            var categoryId = Library.Categories.Single(c => c.Name == "Development").Id;
+            var groups = Library.Books
+                                .Where(b => b.CategoryId == categoryId)
+                                .GroupBy(b => b.PublishedYear)
+                                .OrderBy(b => b.Key);
 
+            foreach(var group in groups) {
+                Console.WriteLine("#{0}年", group.Key);
+                foreach(var book in group) {
+                    Console.WriteLine(" {0}", book.Title);
+                }
+            }
         }
 
         private static void Exercise1_8() {
-
+            var query = Library.Categories
+                               .GroupJoin(Library.Books,
+                                   c => c.Id,
+                                   b => b.CategoryId,
+                                  (c, b) => new {
+                                      CategoryName = c.Name,
+                                      Count = b.Count()
+                                  })
+                               .Where(x => x.Count >= 4);
+            foreach(var category in query) {
+                Console.WriteLine(category.CategoryName);
+            }
         }
     }
 }
